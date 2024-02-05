02d0292bdf580d7d94529259f975964fefc3c7a1
eg-delacruz <netoxas3107@hotmail.com>
2024-02-05 21:05:44 +0100

    Needed configurations to deploy server to Vercel done

--- api/index.js
+++ api/index.js
@@ -33,11 +33,11 @@ const options = {
 //Importante que esto vaya antes del routerApi(app)
 app.use(cors(options));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Hola mi server en express');
 });
 
-app.get('/nueva-ruta', (req, res) => {
+app.get('/api/nueva-ruta', (req, res) => {
   res.send('Hola , soy una nueva ruta');
 });
 
